f06b1543831d945e5026ab8f8caf21503811065b
minorscarf <minorscarf@gmail.com>
2024-10-01 02:04:05 +0900

    テスト成功スレッドスレッドエヴァエヴァエヴァエヴァエヴァエヴァ

--- src/Thread.jsx
+++ src/Thread.jsx
@@ -1,9 +1,23 @@
-export const Thread = () => {
+import { useEffect } from "react";
+import { useState }  from  "react";
 
-    
+export const Thread = () => {
+    const [threadTopic, setThreadTopic] = useState();
+    const baseUrl  = "https://railway.bulletinboard.techtrain.dev"; 
+    const getThreadPoint = "/threads"; 
+
+    useEffect(() => {
+        fetch(baseUrl + getThreadPoint)
+        .then(res => res.json())
+        .then(data => {
+            const titles = data.map(thread => thread.title);
+            setThreadTopic(titles);
+        })
+    },[]);
+
     return(
         <section>
-            <p>Thread</p>
+            <p>{threadTopic}</p>
         </section>
     );
 };
